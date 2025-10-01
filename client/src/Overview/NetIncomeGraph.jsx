9c82c862790ac05260391f196618a65fe93f8164
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2025-10-01 10:58:22 -0500

    style(TotalRevenueGraph, NetIncomeGraph components): make graph looks nicer

--- client/src/Overview/NetIncomeGraph.jsx
+++ client/src/Overview/NetIncomeGraph.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import styles from "./Overview.module.css"
 
 function NetIncomeGraph(props) {
+
+    function countDigits(value) {
+        if (value === 0) {
+            return 1;
+        }
+        return Math.floor(Math.log10(Math.abs(value))) + 1;
+    }
+
+    const desiredTickCount = 5;
+    const xAxisInterval = Math.floor(props.reports.length / desiredTickCount);
+
     return (
-        <ResponsiveContainer width="33%" height={400}>
-            <BarChart
-                data={props.reports}
-                margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="netIncome" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-            </BarChart>
-        </ResponsiveContainer>
+        <div className={styles.graph}>
+            <h2>Net Income</h2>
+            <ResponsiveContainer width="100%" height={400}>
+                <BarChart
+                    data={props.reports}
+                    margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date" stroke="#DAD7CD" interval="equidistantPreserveStart" tick={{fontSize: 12}} />
+                    <YAxis tickFormatter={(value) => {
+                        if (countDigits(value) >= 10) {return `${value / 1000000000}B`}
+                        if (countDigits(value) >= 7) {return `${value / 1000000}M`}
+                        return value
+                    }} stroke="#DAD7CD" />
+                    <Tooltip formatter={(value) => {
+                        if (countDigits(value) >= 10) {return `${value / 1000000000}B`}
+                        if (countDigits(value) >= 7) {return `${value / 1000000}M`}
+                        return value
+                    }}/>
+                    <Bar dataKey="netIncome" fill="#A3B18A" activeBar={<Rectangle fill="#588157" stroke="#DAD7CD" />} />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
     )
 }
 
--- client/src/Overview/TotalRevenueGraph.jsx
+++ client/src/Overview/TotalRevenueGraph.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import styles from "./Overview.module.css"
 
 function TotalRevenueGraph(props) {
+
+    function countDigits(value) {
+        if (value === 0) {
+            return 1;
+        }
+        return Math.floor(Math.log10(Math.abs(value))) + 1;
+    }
+
     return (
-        <ResponsiveContainer width="33%" height={400}>
-            <BarChart
-                data={props.reports}
-                margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="totalRevenue" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-            </BarChart>
-        </ResponsiveContainer>
+        <div className={styles.graph}>
+            <h2>Revenue</h2>
+            <ResponsiveContainer width="100%" height={400}>
+                <BarChart
+                    data={props.reports}
+                    margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date" interval="equidistantPreserveStart" stroke="#DAD7CD" tick={{fontSize: 12}} />
+                    <YAxis tickFormatter={(value) => {
+                        if (countDigits(value) >= 10) {return `${value / 1000000000}B`}
+                        if (countDigits(value) >= 7) {return `${value / 1000000}M`}
+                        return value
+                    }} stroke="#DAD7CD"/>
+                    <Tooltip formatter={(value) => {
+                        if (countDigits(value) >= 10) {return `${value / 1000000000}B`}
+                        if (countDigits(value) >= 7) {return `${value / 1000000}M`}
+                        return value
+                    }}/>
+                    <Bar dataKey="totalRevenue" fill="#A3B18A" activeBar={<Rectangle fill="#588157" stroke="#DAD7CD" />} />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
     )
 }
 
