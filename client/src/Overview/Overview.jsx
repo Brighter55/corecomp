80333ebcb6b9eff70c053d413974e32411018240
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2025-10-20 10:03:08 -0500

    feat(Overview): handleSearchSubmit now refreshes tokens when access expires

--- client/src/Overview/Overview.jsx
+++ client/src/Overview/Overview.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useState, useRef} from "react"
 import {useNavigate} from "react-router-dom"
 import {useEffect} from "react"
 import AppHeader from "../AppHeader/AppHeader.jsx"
@@ -22,66 +22,108 @@ import logo from "../assets/logoPlaceholder.png"
 
 function Overview() {
     // when opens up the page check if user is authorized
-    // TODO: handle when the the token expires
+    // TODO: implement "refresh lock" in prod. to prevent multiple components hitting refreshing the tokens
     const navigate = useNavigate();
+    const ran = useRef(false);
 
-    useEffect(() => {
-        async function isAuthorized() {
-            const response = await fetch("http://127.0.0.1:8000/api/is-authorized", {
-                method: "POST",
-                headers: {
-                    "Authorization": `Bearer ${sessionStorage.getItem("access")}`,
-                },
-            });
-            const data = await response.json();
-
-            if (data.messages[0].message === "Token is expired") {
-                const response = await fetch("http://127.0.0.1:8000/api/refresh", {
+        useEffect(() => {
+            async function isAuthorized() {
+                const response = await fetch("http://127.0.0.1:8000/api/is-authorized", {
                     method: "POST",
                     headers: {
-                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${sessionStorage.getItem("access")}`,
                     },
-                    body: JSON.stringify({refresh: sessionStorage.getItem("refresh")})
                 });
-                if (response.status === 403) { /*Handle user's trial expires and not subscribe*/
-                    console.log("exceed trial_expiry and not pay, navigate to payment page");
-                    return;
-                } else if (!response.ok) { /*Refresh expires in general needs log in again*/
-                    console.log("refresh is invalid");
-                    navigate("/sign-in");
-                    return;
-                }
-
                 const data = await response.json();
-                sessionStorage.setItem("access", data.access);
-                sessionStorage.setItem("refresh", data.refresh);
-                console.log(`recieved new pair of tokens. {access: ${sessionStorage.getItem("access")}, refresh: ${sessionStorage.getItem("refresh")}`);
-            } else if (!response.ok) {
-                navigate("/sign-up");
-            }
-        }
 
-        isAuthorized();
-    }, []);
+                /*get new tokens if access expires*/
+                if (!response.ok) {
+                    if (data?.messages?.[0]?.message === "Token is expired") {
+                        const response = await fetch("http://127.0.0.1:8000/api/refresh", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({refresh: sessionStorage.getItem("refresh")})
+                        });
+                        if (response.status === 403) { /*Handle user's trial expires and not subscribe*/
+                            console.log("exceed trial_expiry and not pay, navigate to payment page");
+                            return;
+                        } else if (!response.ok) { /*Refresh expires in general needs log in again*/
+                            console.log("refresh is invalid");
+                            navigate("/sign-in");
+                            return;
+                        }
+
+                        const data = await response.json();
+                        sessionStorage.setItem("access", data.access);
+                        sessionStorage.setItem("refresh", data.refresh);
+                        console.log(`recieved new pair of tokens. {access: ${sessionStorage.getItem("access")}, refresh: ${sessionStorage.getItem("refresh")}`);
+                    } else {
+                        navigate("/sign-up");
+                    }
+                }
+            }
+            /*prevent isAuthorized to run second time in developement*/
+            if (ran.current) {return;}
+            ran.current = true;
+            isAuthorized();
+        }, []);
 
     const [symbol, setSymbol] = useState("");
     const [period, setPeriod] = useState("annually");
     const [reports, setReports] = useState([]);
 
-    async function handleSearchSubmit(event) {
-        event.preventDefault();
-
+    async function getReports() {
         const payload = {symbol: symbol, period: period};
-        try {
-            const response = await fetch("http://127.0.0.1:8000/api/overview", {
+        const response = await fetch("http://127.0.0.1:8000/api/overview", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                     "Authorization": `Bearer ${sessionStorage.getItem("access")}`,
                 },
                 body: JSON.stringify(payload),
-            });
-            const data = await response.json();
+        });
+        return response
+    }
+
+    async function handleSearchSubmit(event) {
+        event.preventDefault();
+        try {
+            let response = await getReports();
+            let data = await response.json();
+
+            /*get new tokens if access expires*/
+            if (!response.ok) {
+                if (data?.messages?.[0]?.message === "Token is expired") {
+                    const refreshResponse = await fetch("http://127.0.0.1:8000/api/refresh", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({refresh: sessionStorage.getItem("refresh")})
+                    });
+                    if (refreshResponse.status === 403) { /*Handle user's trial expires and not subscribe*/
+                        console.log("exceed trial_expiry and not pay, navigate to payment page");
+                        return;
+                    } else if (!refreshResponse.ok) { /*Refresh expires in general needs log in again*/
+                        console.log("refresh is invalid");
+                        navigate("/sign-in");
+                        return;
+                    }
+
+                    const tokens = await refreshResponse.json();
+                    sessionStorage.setItem("access", tokens.access);
+                    sessionStorage.setItem("refresh", tokens.refresh);
+                    console.log(`recieved new pair of tokens. {access: ${sessionStorage.getItem("access")}, refresh: ${sessionStorage.getItem("refresh")}`);
+                    response = await getReports();
+                    data = await response.json();
+                } else {
+                    navigate("/sign-up");
+                }
+            }
+
+
             setReports(data);
             console.log(data);
         } catch (error) {
