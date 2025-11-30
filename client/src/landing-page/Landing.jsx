55ebe27efacf669f448163be1c9bdffa401e8b05
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2025-11-30 02:18:00 -0600

    feat(Landing component): add explanation feature

--- client/src/landing-page/Landing.jsx
+++ client/src/landing-page/Landing.jsx
@@ -7,7 +7,7 @@ import IconButton from '@mui/material/IconButton';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Grow from '@mui/material/Grow';
-import placeholderImage from "../assets/placeholderImage.png"
+import explanationImage from "../assets/explanation.png"
 import SampleIncomeGraph from "./components/sample-income-graph/SampleIncomeGraph.jsx"
 import GraphsCarousel from "./components/graphs-carousel/GraphsCarousel"
 import styles from "./Landing.module.css"
@@ -20,7 +20,7 @@ function Landing() {
     const [features, setFeatures] = useState([
         { id: 0, text: "Over 20 years of comprehensive financial data that provides a broad view of any company.", content: <SampleIncomeGraph /> },
         { id: 1, text: "10+  fundamentals to help you truly understand a company’s performance", content: <GraphsCarousel/> },
-        { id: 2, text: "Beginners? Professionals? CoreComp explains everything in simple terms!", content: <img src={placeholderImage} style={{width: "80%", height: "100%",}} /> },
+        { id: 2, text: "Beginners? Professionals? CoreComp explains everything in simple terms!", content: <img src={explanationImage} className={styles.featureImage} /> },
     ]);
     const [currentPage, setCurrentPage] = useState(0);
     const [slideDirection, setSlideDirection] = useState("left");
