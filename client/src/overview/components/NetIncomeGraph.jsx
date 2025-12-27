1102ba187ca8e678d8e0140c2bf6dcf834871bc1
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2025-12-27 02:09:43 -0600

    refactor(SampleIncomeGraph): clean some code up

--- client/src/overview/components/NetIncomeGraph.jsx
+++ client/src/overview/components/NetIncomeGraph.jsx
@@ -12,6 +12,40 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 
+export const explanation = (
+    <Stack spacing={2}>
+        <Typography variant="explanationTopic">What is it?</Typography>
+        <Typography variant="explanationText">
+            Net income is a company's profit after all expenses and taxes have been deducted from its total revenue.
+        </Typography>
+        <Typography variant="explanationTopic">Calculation</Typography>
+        <Typography
+            variant="explanationText"
+            sx={{ fontFamily: "'Times New Roman', Times, 'Segoe Ui', Arial, sans-serif" }}
+        >
+            Net Income = Total Revenue – Total Expenses
+        </Typography>
+        <Typography variant="explanationTopic">Interpretation</Typography>
+        <Stack direction="row" spacing={1}>
+            <TrendingUpIcon
+                sx={{ color: "green", bgcolor: "white", borderRadius: "10px",}}
+            />
+            <Typography variant="explanationText">
+                A positive trend shows the company is improving its financial performance.
+            </Typography>
+        </Stack>
+        <Stack direction="row" spacing={1}>
+            <TrendingDownIcon
+                sx={{ color: "red", bgcolor: "white", borderRadius: "10px",}}
+            />
+            <Typography variant="explanationText">
+                A negative trend can be a warning sign, even if it remains positive. It suggests the company may be facing challenges in its operations or market.
+            </Typography>
+        </Stack>
+    </Stack>
+)
+
+
 function NetIncomeGraph(props) {
     const [timeRange, setTimeRange] = useState("all");
     const reports = filterReports(props.reports, timeRange);
@@ -39,38 +73,6 @@ function NetIncomeGraph(props) {
         }
         return Math.floor(Math.log10(Math.abs(value))) + 1;
     }
-
-    const explanation = (
-        <Stack spacing={2}>
-            <Typography variant="explanationTopic">What is it?</Typography>
-            <Typography variant="explanationText">
-                Net income is a company's profit after all expenses and taxes have been deducted from its total revenue.            </Typography>
-            <Typography variant="explanationTopic">Calculation</Typography>
-            <Typography
-                variant="explanationText"
-                sx={{ fontFamily: "'Times New Roman', Times, 'Segoe Ui', Arial, sans-serif" }}
-            >
-                Net Income = Total Revenue – Total Expenses
-            </Typography>
-            <Typography variant="explanationTopic">Interpretation</Typography>
-            <Stack direction="row" spacing={1}>
-                <TrendingUpIcon
-                    sx={{ color: "green", bgcolor: "white", borderRadius: "10px",}}
-                />
-                <Typography variant="explanationText">
-                    A positive trend shows the company is improving its financial performance.
-                </Typography>
-            </Stack>
-            <Stack direction="row" spacing={1}>
-                <TrendingDownIcon
-                    sx={{ color: "red", bgcolor: "white", borderRadius: "10px",}}
-                />
-                <Typography variant="explanationText">
-                    A negative trend can be a warning sign, even if it remains positive. It suggests the company may be facing challenges in its operations or market.
-                </Typography>
-            </Stack>
-        </Stack>
-    )
 
     return (
         <Box sx={{ flex: 1 }}>
