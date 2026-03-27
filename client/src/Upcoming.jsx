bad50e970925d00f0e82026dd6b616b8d7b725b2
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2026-03-26 23:21:50 -0500

    update upcoming for new version

--- client/src/Upcoming.jsx
+++ client/src/Upcoming.jsx
@@ -12,11 +12,21 @@ function Upcoming() {
                 <LandingHeader></LandingHeader>
                 <Stack spacing={3}>
                     <Stack spacing={2}>
-                        <Typography variant="h5" fontWeight="bold">Upcoming Version: 0.2.0 (???)</Typography>
-                        <Typography variant="body1"> - more graphs</Typography>
+                        <Typography variant="h5" fontWeight="bold">Upcoming Version: 0.3.0 (???)</Typography>
+                        <Typography variant="body1"> - more features</Typography>
                     </Stack>
                     <Stack spacing={2}>
-                        <Typography variant="h5" fontWeight="bold">Current Version: 0.1.0 (3/9/2026)</Typography>
+                        <Typography variant="h5" fontWeight="bold">Current Version: 0.2.0 (3/26/2026)</Typography>
+                        <Stack spacing={1}>
+                            <Typography variant="body1"> - EBIT graph</Typography>
+                            <Typography variant="body1"> - EBITDA graph</Typography>
+                            <Typography variant="body1"> - Return on Equity graph</Typography>
+                            <Typography variant="body1"> - P/E ratio graph</Typography>
+                            <Typography variant="body1"> - P/B ratio graph</Typography>
+                        </Stack>
+                    </Stack>
+                    <Stack spacing={2}>
+                        <Typography variant="h5" fontWeight="bold">Version: 0.1.0 (3/9/2026)</Typography>
                         <Typography variant="body1"> - first deployment</Typography>
                     </Stack>
                 </Stack>
