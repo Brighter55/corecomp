0eab5da382cf034ffa4b0dc8dac079904b4304ac
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2026-04-29 11:44:51 -0500

    migration: uninstall mui package

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,74 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles';
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthProvider.jsx';
 import { ThemeProvider as CoreThemeProvider } from './theme/ThemeContext.jsx';
 
-let theme = createTheme({
-  typography: {
-    fontFamily: [
-      "'Segoe Ui'",
-      "Arial",
-      "sans-serif",
-    ].join(","),
-    h2: {
-      fontWeight: "bold",
-    },
-    h3: {
-      fontWeight: "bold",
-    },
-    h4: {
-      fontWeight: "bold",
-    },
-    h5: {
-      fontWeight: "bold",
-    },
-    explanationTopic: {
-      fontWeight: "bold",
-      fontSize: "1rem",
-
-      [createTheme().breakpoints.up("lg")]: {
-        fontSize: "1.5rem",
-      },
-    },
-    explanationText: { // here
-      fontSize: ".8rem",
-      lineHeight: "1.5rem",
-      [createTheme().breakpoints.up("lg")]: {
-        fontSize: "1rem",
-      },
-    },
-  },
-  components: {
-    MuiLink: {
-      defaultProps: {
-        underline: "none",
-      },
-    },
-    MuiFab: {
-      styleOverrides: {
-        root: {
-          zIndex: 0,
-        }
-      },
-    },
-  },
-});
-theme = responsiveFontSizes(theme);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID} >
-        <CoreThemeProvider>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </CoreThemeProvider>
-      </GoogleOAuthProvider>
-    </ThemeProvider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <CoreThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </CoreThemeProvider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
